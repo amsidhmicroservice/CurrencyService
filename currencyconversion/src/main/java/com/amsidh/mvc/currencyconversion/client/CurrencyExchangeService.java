02faf3ce3ed57319a19f08cbc0af4f8d4efed409
Amsidh Lokhande <amsidhlokhande@gmail.com>
2021-08-30 20:15:46 +0530

    Auto generated environment variables does not worked

--- currencyconversion/src/main/java/com/amsidh/mvc/currencyconversion/client/CurrencyExchangeService.java
+++ currencyconversion/src/main/java/com/amsidh/mvc/currencyconversion/client/CurrencyExchangeService.java
@@ -5,8 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-//@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_URI:http://localhost}:8181")
-@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8181")
+@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_URI:http://localhost}:8181")
+//@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8181") //It is not working
 public interface CurrencyExchangeService {
     //http://localhost:8181/currency-exchange/USD/to/INR
     @GetMapping(value = "/currency-exchange/{currencyFrom}/to/{currencyTo}")
