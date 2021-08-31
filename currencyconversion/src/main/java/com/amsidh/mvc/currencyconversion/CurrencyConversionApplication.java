d2d98b0997454af67072c4558ab65ba7b2462d3e
Amsidh Lokhande <amsidhlokhande@gmail.com>
2021-08-31 09:02:37 +0530

    Revert "Added client side load balancer- ribbon"
    This reverts commit 7f21b040a3dbe126f947bcf968a5d0cd344585ca.

--- currencyconversion/src/main/java/com/amsidh/mvc/currencyconversion/CurrencyConversionApplication.java
+++ currencyconversion/src/main/java/com/amsidh/mvc/currencyconversion/CurrencyConversionApplication.java
@@ -2,12 +2,10 @@ package com.amsidh.mvc.currencyconversion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableDiscoveryClient //Change-1
 public class CurrencyConversionApplication {
 
     public static void main(String[] args) {
--- currencyconversion/src/main/java/com/amsidh/mvc/currencyconversion/client/CurrencyExchangeService.java
+++ currencyconversion/src/main/java/com/amsidh/mvc/currencyconversion/client/CurrencyExchangeService.java
@@ -1,15 +1,12 @@
 package com.amsidh.mvc.currencyconversion.client;
 
 import com.amsidh.mvc.currencyconversion.client.response.Exchange;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-//@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_URI:http://localhost}:8181")
+@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_URI:http://localhost}:8181")
 //@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8181") //It is not working
-@FeignClient(name = "currency-exchange") //Change-1
-@RibbonClient(name = "currency-exchange") //Change-1
 public interface CurrencyExchangeService {
     //http://localhost:8181/currency-exchange/USD/to/INR
     @GetMapping(value = "/currency-exchange/{currencyFrom}/to/{currencyTo}")
