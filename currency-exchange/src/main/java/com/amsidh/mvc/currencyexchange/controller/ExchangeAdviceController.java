13babcad3e130d64563f2719c19046414c8b52d8
amsidh.lokhande@xebia.com <amsidh.lokhande@xebia.com>
2022-05-22 15:22:13 +0530

    Configure spring-boot-maven-plugin for image creation instead of fabric8

--- currency-exchange/src/main/java/com/amsidh/mvc/currencyexchange/controller/ExchangeAdviceController.java
+++ currency-exchange/src/main/java/com/amsidh/mvc/currencyexchange/controller/ExchangeAdviceController.java
@@ -2,7 +2,6 @@ package com.amsidh.mvc.currencyexchange.controller;
 
 import com.amsidh.mvc.currencyexchange.exception.ErrorMessage;
 import com.amsidh.mvc.currencyexchange.exception.MyCustomException;
-import io.fabric8.kubernetes.client.ResourceNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,19 +16,6 @@ import static net.logstash.logback.argument.StructuredArguments.kv;
 @Slf4j
 @RestControllerAdvice
 public class ExchangeAdviceController {
-
-    @ExceptionHandler(ResourceNotFoundException.class)
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public ErrorMessage resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-        ErrorMessage message = ErrorMessage.builder()
-                .description(request.getDescription(false))
-                .message(ex.getMessage())
-                .statusCode(HttpStatus.NOT_FOUND.value())
-                .timestamp(new Date())
-                .build();
-        log.error(message.toString(), kv("status", message.getStatusCode()), kv("exception", ex.getStackTrace()));
-        return message;
-    }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
