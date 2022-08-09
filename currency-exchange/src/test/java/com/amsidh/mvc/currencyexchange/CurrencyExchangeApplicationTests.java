2da79bd44e502105399ded64ee815a59bf6d64dd
amsidh.lokhande@xebia.com <amsidh.lokhande@xebia.com>
2022-08-09 11:10:33 +0530

    Sync with Practice-1 branch

---
+++ currency-exchange/src/test/java/com/amsidh/mvc/currencyexchange/CurrencyExchangeApplicationTests.java
@@ -0,0 +1,15 @@
+
+package com.amsidh.mvc.currencyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
