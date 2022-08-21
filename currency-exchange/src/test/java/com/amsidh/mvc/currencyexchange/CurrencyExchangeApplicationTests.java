6501410b5bf2b2aa9d7963eb88f5de09757b6dec
amsidh.lokhande@xebia.com <amsidh.lokhande@xebia.com>
2022-08-21 17:03:46 +0530

    Upgraded sonar version
    Signed-off-by: amsidh.lokhande@xebia.com <amsidh.lokhande@xebia.com>

--- currency-exchange/src/test/java/com/amsidh/mvc/currencyexchange/CurrencyExchangeApplicationTests.java
+++ currency-exchange/src/test/java/com/amsidh/mvc/currencyexchange/CurrencyExchangeApplicationTests.java
@@ -1,14 +1,22 @@
 
 package com.amsidh.mvc.currencyexchange;
 
+import com.amsidh.mvc.currencyexchange.controller.ExchangeController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CurrencyExchangeApplicationTests {
 
+    @Autowired
+    ExchangeController exchangeController;
+
     @Test
     void contextLoads() {
+        CurrencyExchangeApplication.main(new String[]{});
+        Assertions.assertNotNull(exchangeController);
     }
 
 }
