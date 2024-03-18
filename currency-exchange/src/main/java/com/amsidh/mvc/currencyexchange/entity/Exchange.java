4e8b4a7367eb30698ef53cb94217def582024a3d
amsidhmicroservice <amsidhmicroservice@gmail.com>
2024-03-18 12:20:49 +0530

    Java 17

--- currency-exchange/src/main/java/com/amsidh/mvc/currencyexchange/entity/Exchange.java
+++ currency-exchange/src/main/java/com/amsidh/mvc/currencyexchange/entity/Exchange.java
@@ -1,10 +1,10 @@
 package com.amsidh.mvc.currencyexchange.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Data
